b41218ec1d392a3317234bf81d24f3b2c70e3a27
Gapjin Yang <dkvowk@naver.com>
2025-03-23 12:54:29 +0900

    Add Color Theme 추가

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,23 @@
 import Router from "./Router";
 import {Outlet} from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor}; 
+`;
+const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
 
 function Root() {
   return (
       <div>
-          <Header />,
-          <Outlet />
+          <Container>
+              <H1>Protected</H1>
+              <Header />,
+              <Outlet />
+          </Container>
       </div>
   );
 }
